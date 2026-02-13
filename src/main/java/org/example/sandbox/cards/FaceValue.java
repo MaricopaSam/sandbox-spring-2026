b2db41853131b8fd33ce600088aebdab218fcce1
ClawedMaple <samuel.kuether@gmail.com>
2026-02-12 18:35:30 -0800

    Adding Card Classes

---
+++ src/main/java/org/example/sandbox/cards/FaceValue.java
@@ -0,0 +1,5 @@
+package org.example.sandbox.cards;
+
+public enum FaceValue {
+    ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING
+}
---
+++ src/main/java/org/example/sandbox/cards/Suit.java
@@ -0,0 +1,9 @@
+package org.example.sandbox.cards;
+
+public enum Suit {
+
+    HEARTS, CLUBS, DIAMONDS, SPADES
+
+
+
+}
